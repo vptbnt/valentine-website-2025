9972f1fdf9776770cbdc9e00fd2c25d1327b1087
vptbnt <vpt.beneteau@gmail.com>
2026-02-01 18:26:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "💖 Veux tu être ma Valentine ? 💖 ",
+    pageTitle: "Projet secret ",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org

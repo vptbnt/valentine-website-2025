311dd3331bc7bec8c9c1eaf8959ee2910ad16873
vptbnt <vpt.beneteau@gmail.com>
2026-02-01 18:01:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const CONFIG = {
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
-        hearts: ['❤️', '💖', '💝','💋', '💗', '💓','💕'],  // Heart emojis
+        hearts: ['❤️', '💖', '💝','💋', '💗', '💓','💕','💘'],  // Heart emojis
         bears: ['🧸', '🐻','🦄','💋','🌹']                       // Cute bear emojis
     },
 
